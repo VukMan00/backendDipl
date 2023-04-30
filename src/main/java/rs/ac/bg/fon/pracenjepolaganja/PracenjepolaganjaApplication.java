65751e9f07302bfbe49d3d735415338e27ed8a4d
Vuk Manojlovic <vukman619@gmail.com>
2023-04-30 11:14:30 +0200

    Napravljen Maven projekat

---
+++ src/main/java/rs/ac/bg/fon/pracenjepolaganja/PracenjepolaganjaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.pracenjepolaganja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracenjepolaganjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracenjepolaganjaApplication.class, args);
+	}
+
+}
